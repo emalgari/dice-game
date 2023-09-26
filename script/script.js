d8a823222db1399214d26f41ffaa714a0ae373cf
saddam <saddam.hussain@gmx.com>
2023-09-26 20:59:33 +0200

    Initialize DOM selection and game states

---
+++ script/script.js
@@ -0,0 +1,20 @@
+//DOM
+const playerTurn = document.querySelector(".whos-turn");
+const titles = document.querySelectorAll(".title");
+const player1Score = document.querySelector(".player1-score");
+const player1TextColor = document.querySelector(".player-1-data");
+const player2TextColor = document.querySelector(".player-2-data");
+const player2Score = document.querySelector(".player2-score");
+const player1Turns = document.querySelector(".player1-turnsLeft");
+const player2Turns = document.querySelector(".player2-turnsLeft");
+const playButton = document.querySelector(".play-btn");
+const gameResult = document.querySelector(".result");
+const mainContainer = document.querySelector(".main-container");
+
+//game states
+let turn = 0;
+let currentPlayer = 1;
+let player1CurrentScore = 0;
+let player2CurrentScore = 0;
+let playerOneCurrentTurns = 10;
+let playerTwoCurrentTurns = 10;
